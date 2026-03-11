3ffc8cba7cdfdf49916f14d41e59f9df288b87f2
bosswaza <ewan.droneau@gmail.com>
2026-03-11 14:51:36 +0100

    Debug deno

--- client/src/page/Log.tsx
+++ client/src/page/Log.tsx
@@ -1,1 +1,16 @@
-import "./Log.css";
+import React from 'react';
+
+const Log: React.FC = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Historique des logs</h1>
+      <button onClick={() => window.history.back()}>Retour au Kanban</button>
+      <ul>
+        <li>Log 1: Action effectuée à 10:00</li>
+        <li>Log 2: Action effectuée à 11:00</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Log;
