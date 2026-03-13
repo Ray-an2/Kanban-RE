5f7d76ad1e82a89b1c7e7065fe13ef4dc94681b7
bosswaza <ewan.droneau@gmail.com>
2026-03-13 09:06:01 +0100

    légère modif des routes (non fonctionnelles depuis authentification)

--- client/src/page/App.tsx
+++ client/src/page/App.tsx
@@ -1,32 +1,96 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import KanbanBoard from '../components/Tableau.tsx';
-import Log from './Log.tsx';
-import Inscription from './Inscription.tsx';
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+//import { AuthProvider } from "./contexts/AuthProvider";
+import KanbanBoard from "../components/Tableau.tsx";
+import Log from "./Log.tsx";
+import Inscription from "./Inscription.tsx";
 import Connexion from "./Connexion.tsx";
 import TableauPage from "./Accueil.tsx";
-import './App.css';
 import CardDetailsPage from "./Carte.tsx";
 import AccountPage from "./Compte.tsx";
 import Notification from "./Notification.tsx";
+/*import { RestrictedGuest } from "./components/RestrictedGuest";
+import { RestrictedLoggedIn } from "./components/RestrictedLoggedIn";*/
 
-const App: React.FC = () => {
+import "./App.css";
+
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/tableau/:id" element={<KanbanBoard />} />
-        <Route path="/tableau/:id/log" element={<Log />} />
-        <Route path="/inscription" element={<Inscription />} />
-        <Route path="/login" element={<Connexion />} />
-        <Route path="/tableau" element={<TableauPage />} />
-        <Route path="/tableau/:id/carte/:id" element={<CardDetailsPage />} />
-        <Route path="/compte/:pseudo" element={<AccountPage />} />
-        <Route path="/notification" element={<Notification />} />
-        <Route path="*" element={<h1>Page non trouvée</h1>} />
-      </Routes>
-    </Router>
+    //<AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+
+          <Route
+            path="/login"
+            element={
+              //<RestrictedGuest>
+                <Connexion />
+              //</RestrictedGuest>
+            }
+          />
+          <Route
+            path="/inscription"
+            element={
+              //<RestrictedGuest>
+                <Inscription />
+              //</RestrictedGuest>
+            }
+          />
+
+          <Route
+            path="/tableau"
+            element={
+              //<RestrictedLoggedIn>
+                <TableauPage />
+              //</RestrictedLoggedIn>
+            }
+          />
+          <Route
+            path="/tableau/:id"
+            element={
+              //<RestrictedLoggedIn>
+                <KanbanBoard />
+              //</RestrictedLoggedIn>
+            }
+          />
+          <Route
+            path="/tableau/:id/log"
+            element={
+              //<RestrictedLoggedIn>
+                <Log />
+              //</RestrictedLoggedIn>
+            }
+          />
+          <Route
+            path="/tableau/:boardId/carte/:cardId"
+            element={
+              //<RestrictedLoggedIn>
+                <CardDetailsPage />
+              //</RestrictedLoggedIn>
+            }
+          />
+          <Route
+            path="/compte"
+            element={
+              //<RestrictedLoggedIn>
+                <AccountPage />
+              //</RestrictedLoggedIn>
+            }
+          />
+          <Route
+            path="/notification"
+            element={
+              //<RestrictedLoggedIn>
+                <Notification />
+              //</RestrictedLoggedIn>
+            }
+          />
+
+          <Route path="*" element={<h1>Page non trouvée</h1>} />
+        </Routes>
+      </BrowserRouter>
+    //</AuthProvider>
   );
-};
+}
 
 export default App;
