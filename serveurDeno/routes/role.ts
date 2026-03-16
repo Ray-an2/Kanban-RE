8e3f7ea4655c711ad5916bd354deb70d2ded9c4b
Ray-an2 <brossardrayan.rb@gmail.com>
2026-03-16 09:19:58 +0100

    Architecture des routes à faire rediriger

---
+++ serveurDeno/routes/role.ts
@@ -0,0 +1,6 @@
+import { Router } from "@oak/oak";
+import { db } from "../main.ts";
+
+import { APIErreurCode, APIException, APIResponse } from "../model/reponse.ts";
+
+const router = new Router({ prefix: "/role" });
