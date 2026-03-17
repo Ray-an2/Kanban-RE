6f40aba3039f0abfec1f86775b8372912eb0f813
bosswaza <ewan.droneau@gmail.com>
2026-03-17 09:47:29 +0100

    changement du pom pour régler les erreurs 2

---
+++ serveurTomcat/api.journal/src/main/java/com/kanban/journal/service/impl/JournalServiceImpl.java
@@ -0,0 +1,33 @@
+package com.kanban.journal.service.impl;
+
+import com.kanban.journal.dtos.JournalDto;
+
+public class JournalServiceImpl extends JournalService {
+    JournalDto createJournal(JournalDto journalDto) {
+        return null;
+    }
+    JournalDto getJournalById(String id) {
+        return null;
+    }
+    JournalDto getJournalByTitle(String title) {
+        return null;
+    }
+    JournalDto getJournalByAuthor(String author) {
+        return null;
+    }
+    JournalDto getJournalByAction(String action) {
+        return null;
+    }
+    JournalDto getJournalByDate(String date) {
+        return null;
+    }
+    JournalDto getJournalByEtat(String etat) {
+        return null;
+    }
+    JournalDto getJournalByTabId(String tabId) {
+        return null;
+    }
+    JournalDto getJournalByCarId(String carId) {
+        return null;
+    }
+}
--- serveurTomcat/target/classes/com/kanban/journal/service/JournalService.java
+++ serveurTomcat/target/classes/com/kanban/journal/service/JournalService.java
@@ -1,7 +1,15 @@
 package com.kanban.journal.service;
 
-import org.springframework.stereotype.Service;
+import com.kanban.journal.dtos.JournalDto;
 
-@Service
 public interface  JournalService {
+    JournalDto createJournal(JournalDto journalDto);
+    JournalDto getJournalById(String id);
+    JournalDto getJournalByTitle(String title);
+    JournalDto getJournalByAuthor(String author);
+    JournalDto getJournalByAction(String action);
+    JournalDto getJournalByDate(String date);
+    JournalDto getJournalByEtat(String etat);
+    JournalDto getJournalByTabId(String tabId);
+    JournalDto getJournalByCarId(String carId);
 }
