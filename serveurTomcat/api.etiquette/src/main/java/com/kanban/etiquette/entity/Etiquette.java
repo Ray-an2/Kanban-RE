e65d1ea910adc9e7c7a956de2f2d445ebf13985f
bosswaza <ewan.droneau@gmail.com>
2026-03-18 10:58:04 +0100

    création de l'entity etiquette pour débug spring

--- serveurTomcat/api.etiquette/src/main/java/com/kanban/etiquette/entity/Etiquette.java
+++ serveurTomcat/api.etiquette/src/main/java/com/kanban/etiquette/entity/Etiquette.java
@@ -1,5 +1,22 @@
 package com.kanban.etiquette.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "t_etiquette_eti")
 public class Etiquette {
 
+    @Id
+    @Column(name = "eti_id")
+    private String id;
+
+    @Column(name = "eti_nom", nullable = false)
+    private String nom;
+
+    @Column(name = "eti_couleur")
+    private String couleur;
 }
