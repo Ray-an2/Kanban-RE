aecad38f03c48c747b1e1578818c12c83e5ebf8d
Ray-an2 <brossardrayan.rb@gmail.com>
2026-03-18 10:20:07 +0100

    Fix lancement Spring

--- serveurTomcat/src/main/java/com/kanban/tomcat/ServletInitializer.java
+++ serveurTomcat/src/main/java/com/kanban/tomcat/ServletInitializer.java
@@ -1,5 +1,6 @@
 package com.kanban.tomcat;
 
+import com.kanban.ServerTomcatApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
