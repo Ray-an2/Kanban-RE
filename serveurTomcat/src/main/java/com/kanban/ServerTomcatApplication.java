9f4c9541d8f73832fff5a607618645e7538eeb85
Ray-an2 <brossardrayan.rb@gmail.com>
2026-03-18 13:31:33 +0100

    Fix de lancement serveurTomcat

--- serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
+++ serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
@@ -3,13 +3,13 @@ package com.kanban;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.kanban")
 @EnableJpaRepositories(basePackages = "com.kanban")
-@EntityScan(basePackages = "com.kanban")
+@EnableMongoRepositories(basePackages = "com.kanban")
 public class ServerTomcatApplication extends SpringBootServletInitializer {
 
     @Override
