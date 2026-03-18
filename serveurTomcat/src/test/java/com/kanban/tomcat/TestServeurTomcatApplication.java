a0e1bd2478bef4f352ae8879219effb20474f7f2
bosswaza <ewan.droneau@gmail.com>
2026-03-18 10:37:24 +0100

    rename du test et débug fini (2 configurations dont 1 mauvaise)

--- serveurTomcat/src/test/java/com/kanban/tomcat/TestServeurTomcatApplication.java
+++ serveurTomcat/src/test/java/com/kanban/tomcat/TestServeurTomcatApplication.java
@@ -1,8 +1,9 @@
 package com.kanban.tomcat;
 
+import com.kanban.ServerTomcatApplication;
 import org.springframework.boot.SpringApplication;
 
-public class TestServeurTomcaApplication {
+public class TestServeurTomcatApplication {
 
     public static void main(String[] args) {
         SpringApplication.from(ServerTomcatApplication::main).with(TestcontainersConfiguration.class).run(args);
