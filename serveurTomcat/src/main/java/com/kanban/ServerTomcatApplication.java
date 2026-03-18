dd6f0c2ebe2976a10d70defa72af14c52179816a
bosswaza <ewan.droneau@gmail.com>
2026-03-18 10:22:45 +0100

    ajout début config pour spring (journal accessible

--- serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
+++ serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
@@ -3,9 +3,13 @@ package com.kanban;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.kanban")
+@EnableJpaRepositories(basePackages = "com.kanban")
+@EntityScan(basePackages = "com.kanban")
 public class ServerTomcatApplication extends SpringBootServletInitializer {
 
     @Override
