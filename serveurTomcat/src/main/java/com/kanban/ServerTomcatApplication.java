2152f04aa8f0ef2b484ee17ae56985d0ec0ad847
Ray-an2 <brossardrayan.rb@gmail.com>
2026-03-19 10:11:54 +0100

    Suppression des dependance non utiliser

--- serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
+++ serveurTomcat/src/main/java/com/kanban/ServerTomcatApplication.java
@@ -8,8 +8,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication(scanBasePackages = "com.kanban")
-@EnableJpaRepositories(basePackages = "com.kanban")
-@EnableMongoRepositories(basePackages = "com.kanban")
+@EnableJpaRepositories(basePackages = {
+        "com.kanban.compte.repository",
+        "com.kanban.profil.repository",
+        "com.kanban.tableau.repository",
+        "com.kanban.role.repository",
+        "com.kanban.liste.repository",
+        "com.kanban.carte.repository",
+        "com.kanban.etiquette.repository",
+        "com.kanban.associer.repository",
+        "com.kanban.membre.repository",
+        "com.kanban.notification.repository",
+        "com.kanban.journal.repository"
+})
+@EnableMongoRepositories(basePackages = {
+        "com.kanban.commentaire.repository",
+        "com.kanban.document.repository"
+})
 public class ServerTomcatApplication extends SpringBootServletInitializer {
 
     @Override
