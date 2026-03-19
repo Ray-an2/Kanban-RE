4d55d82d0e8cc7e690b00a376258344d0a8550ea
bosswaza <ewan.droneau@gmail.com>
2026-03-19 10:29:13 +0100

    Plan de test (en cours)

--- serveurTomcat/api.liste/src/main/java/com/kanban/liste/dtos/ListeDto.java
+++ serveurTomcat/api.liste/src/main/java/com/kanban/liste/dtos/ListeDto.java
@@ -3,13 +3,14 @@ package com.kanban.liste.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.kanban.carte.dtos.CarteDto;
 import lombok.Getter;
-import lombok.Setter;
+<parameter name="file_text">import lombok.Setter;
 
 import java.util.List;
 
 @Setter
 @Getter
 public class ListeDto {
+
     @JsonProperty("lis_id")
     private String id;
 
@@ -27,4 +28,7 @@ public class ListeDto {
 
     @JsonProperty("cartes")
     private List<CarteDto> cartes;
+
+    @JsonProperty("auteur")
+    private String auteur;
 }
--- serveurTomcat/api.liste/src/main/java/com/kanban/liste/service/ListeService.java
+++ serveurTomcat/api.liste/src/main/java/com/kanban/liste/service/ListeService.java
@@ -1,12 +1,26 @@
 package com.kanban.liste.service;
 
 import com.kanban.liste.dtos.ListeDto;
+import com.kanban.liste.dtos.OrdreCarteDto;
+import com.kanban.liste.dtos.OrdreListeDto;
 
 import java.util.List;
 
 public interface ListeService {
+
+    // --- CRUD de base ---
     ListeDto createListe(ListeDto listeDto);
     ListeDto getListeById(String id);
     List<ListeDto> getListesByTabId(String tabId);
+    ListeDto updateListe(String id, ListeDto listeDto);
     boolean deleteListe(String id);
+
+    // --- Archivage ---
+    ListeDto archiverListe(String id);
+    ListeDto desarchiverListe(String id);
+    List<ListeDto> getListesArchivees(String tabId);
+
+    // --- Réorganisation ---
+    void updateOrdreListes(String tabId, List<OrdreListeDto> ordres);
+    void updateOrdreCartes(String lisId, List<OrdreCarteDto> ordres);
 }
