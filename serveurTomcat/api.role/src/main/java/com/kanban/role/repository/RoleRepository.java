c093a338db326b64d5ba0d882c381ec1bc97b407
bosswaza <ewan.droneau@gmail.com>
2026-03-20 15:46:45 +0100

    Retour ancienne version

--- serveurTomcat/api.role/src/main/java/com/kanban/role/repository/RoleRepository.java
+++ serveurTomcat/api.role/src/main/java/com/kanban/role/repository/RoleRepository.java
@@ -1,9 +1,18 @@
+package com.kanban.role.repository;
+
+import com.kanban.role.entity.Role;
+import com.kanban.role.entity.RoleId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role, RoleId> {
 
-  List<Role> findByIdCptId(String cptId);
+  List<Role> findByCptId(String cptId);
 
-  List<Role> findByIdTabId(String tabId);
+  List<Role> findByTabId(String tabId);
 
-  boolean existsByIdCptIdAndIdTabId(String cptId, String tabId);
+  boolean existsByCptIdAndTabId(String cptId, String tabId);
 }
